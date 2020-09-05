0305b54215ce37758e4f1ec60ccd1bb1f55a3420
Jadilson Guedes <jadilson12@gmail.com>
2020-09-05 12:30:40 +0100

    atualizaçao do perfil

--- 09-finalizando-e-teste-no-nodejs/src/shared/infra/routes/index.ts
+++ 09-finalizando-e-teste-no-nodejs/src/shared/infra/routes/index.ts
@@ -4,6 +4,7 @@ import appointmentsRouter from '@modules/appointments/infra/http/routes/appointm
 import sessionRouter from '@modules/users/infra/http/routes/session.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
