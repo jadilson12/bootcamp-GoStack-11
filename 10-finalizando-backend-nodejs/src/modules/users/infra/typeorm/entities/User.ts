e87065d5ffd6a9c7405b7c78729112edc69ac8e1
Jadilson Guedes <jadilson12@gmail.com>
2020-09-10 12:30:40 +0100

    adiciona o drive da s3 da ws

--- 10-finalizando-backend-nodejs/src/modules/users/infra/typeorm/entities/User.ts
+++ 10-finalizando-backend-nodejs/src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+import uploadConfig from '@config/upload';
 @Entity('users')
 export default class User {
   @PrimaryGeneratedColumn('uuid')
@@ -33,8 +34,16 @@ export default class User {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
