cd0c0a2f6b45a11a1b1b25347c821263b654c12f
Jadilson Guedes <jadilson12@gmail.com>
2020-09-10 12:30:40 +0100

    organizando os containes  provider

--- 10-finalizando-backend-nodejs/src/shared/container/providers/index.ts
+++ 10-finalizando-backend-nodejs/src/shared/container/providers/index.ts
@@ -1,30 +1,3 @@
-import { container } from 'tsyringe';
-
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '../providers/StorageProviders/models/IStorageProvider';
-import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProviders/models/IMailProvider';
-import EtherealMailProvider from './MailProviders/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProviders/implementations/SESMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/model/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProviders';
+import './MailTemplateProvider';
+import './MailProviders';
